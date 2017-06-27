47e12d24f6f32be5a18ff8939e53522169e0a858
André Geuze <andre.geuze@avanade.com>
2017-06-27 19:09:32 +0200

    update for release

--- demo-4-deploy/deploy.bash
+++ demo-4-deploy/deploy.bash
@@ -1,1 +1,1 @@
-docker login -u $(__docker.username__) -p $(__docker.password__) $(__docker.registry__) && cd docker-compose && docker-compose pull && docker-compose stop & docker-compose rm -f && docker-compose up -d
+docker login -u __docker.username__ -p "__docker.password__" __docker.registry__ && cd docker-compose && docker-compose pull && docker-compose stop & docker-compose rm -f && docker-compose up -d
